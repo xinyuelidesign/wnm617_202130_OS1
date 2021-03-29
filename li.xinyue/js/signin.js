07b7d33a43e0184580fe86eacbee201b9405f763
李欣悦 <xin@rikinetsus-MacBook-Air.local>
2021-03-29 15:04:13 -0700

    push

--- li.xinyue/js/signin.js
+++ li.xinyue/js/signin.js
@@ -1,30 +1,36 @@
-
+// Check and change data
 const checkSigninForm = () => {
+   
    let username = $("#signin-username").val();
    let password = $("#signin-password").val();
+   // console.log(username, password);
 
-   if(username=="user" && password=="pass") {
+   if(username == "user" && password == "pass") {
       console.log("logged in")
       sessionStorage.userId = 3;
+      
    } else {
       console.log("logged out")
       sessionStorage.removeItem("userId");
-   }
+   };
 
    checkUserId();
-}
+};
 
 
+// Separation of concerns - seperating out every part of application if is possible
+
+// Change page base on data
 const checkUserId = () => {
-   let p = ["#signin-page","#signup-page",""];
+   let p = ["#signin-page", "#signup-page", ""];
 
    if(sessionStorage.userId === undefined) {
-      // not logged in
-      if(!p.some(o=>window.location.hash===o))
+      // not logged in and not on any of page, then navigate 
+      if (!p.some(o=>window.location.hash===o))
          $.mobile.navigate("#signin-page");
    } else {
-      // logged in
-      if(p.some(o=>window.location.hash===o))
+      // logged in and on any of page, then navigate
+      if (p.some(o=>window.location.hash===o))
          $.mobile.navigate("#recent-page");
-   }
-}
+   };
+};
